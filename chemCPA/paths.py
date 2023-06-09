800757793662af9d900ccc697ab4fd609425cafa
Doosheck <michal.adam.kasperek@gmail.com>
2023-06-09 12:00:30 +0000

    ADD. files for pp etc.

--- chemCPA/paths.py
+++ chemCPA/paths.py
@@ -3,7 +3,7 @@ from pathlib import Path
 ROOT = Path(__file__).parent.resolve().parent
 
 PROJECT_DIR = ROOT / "project_folder"
-DATA_DIR = PROJECT_DIR / "datasets"
+DATA_DIR = '/storage/kasperek/sc-transformer/forkCPA/datasets/'
 EMBEDDING_DIR = PROJECT_DIR / "embeddings"
 CHECKPOINT_DIR = PROJECT_DIR / "checkpoints"
 FIGURE_DIR = PROJECT_DIR / "figures"
