c18d5a815b2d5aab315bb072b4dedec0a92a6efb
gocato <goricamps@gmail.com>
2023-09-11 15:20:30 +0200

    update path to use in cluster

--- compare/preprocessing.py
+++ compare/preprocessing.py
@@ -4,6 +4,12 @@ Add a new split to the dataset to be used as a comparisson to Conditional Monge 
 
 import scanpy as sc
 import numpy as np
+import argparse
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", type=str, dest="path", default="/home/thesis/ConditionalMongeGap/Datasets/sciplex_complete_middle_subset.h5ad", help="Path to ")
+args = parser.parse_args()
 
 # Declare variables
 train_split = 0.8
@@ -13,7 +19,7 @@ rng = np.random.default_rng(1337)
 ood = ["Dacinostat", "Givinostat", "Belinostat", "Hesperadin", "Quisinostat", "Alvespimycin", "Tanespimycin", "TAK-901", "Flavopiridol"]
 
 # Load the dataset
-adata = sc.read_h5ad("/home/thesis/ConditionalMongeGap/Datasets/sciplex_complete_middle_subset.h5ad")
+adata = sc.read_h5ad(args.path)
 
 # Add a new split to the dataset to be used as a comparisson to Conditional Monge Gap
 adata.obs["compare_split"] = np.where(
