96971325f2ea1ab579703c28e62cf48f51112d14
gocato <goricamps@gmail.com>
2023-09-22 11:00:40 +0200

    fix bug

--- compare/baseline_experiment_high_dose.py
+++ compare/baseline_experiment_high_dose.py
@@ -1,31 +1,82 @@
 from pathlib import Path
-
-import matplotlib
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import seml
-import os
-
-from matplotlib import pyplot as plt
-matplotlib.style.use("fivethirtyeight")
-matplotlib.style.use("seaborn-talk")
-matplotlib.rcParams["font.family"] = "monospace"
-plt.rcParams["savefig.facecolor"] = "white"
-sns.set_context("poster")
-pd.set_option("display.max_columns", 15)
+from pprint import pprint
+from seml.config import generate_configs, read_config
+from chemCPA.experiments_run import ExperimentWrapper
+from argparse import ArgumentParser
 
 """
 Chem_CPA experiments are susceptible to randomness. In particular, we need to run the same experiment multiple times to ensure best possible evaluation.
 All the experiments are run with the same hyperparameters, but different random seeds.
 The results are then averaged over the runs (bagging)
 """
-for num in range(0, 5):
-    os.system(
-                "/dss/dsshome1/0A/di93hoq/miniconda3/envs/run_chem_cpa/bin/python /mnt/forkCPA/compare/train.py"
-                + "--path=/mnt/forkCPA/"
-                + "--conig=/mnt/forkCPA/compare/baseline_experiment_highest_dose.yaml"
-                + f"--save=/mnt/forkCPA/compare/checkpoints/trained_model_{num}.pt"
-            )
+parser = ArgumentParser(
+    prog="train.py",
+    description="Train ChemCPA",
+)
 
+parser.add_argument(
+    "--path",
+    dest="path",
+    type=str,
+    help="Path to the projects folder.",
+)
+
+parser.add_argument(
+    "--config",
+    dest="config",
+    type=str,
+    help="Path to the config file.",
+)
+
+parser.add_argument(
+    "--num_trainings",
+    dest="num_trainings",
+    type=int,
+    help="Number of trainings to run.",
+)
+
+parser.add_argument(
+    "--save",
+    dest="save",
+    type=str,
+    help="Path and name of file to save the model.",
+)
+fargs = parser.parse_args()
+
+exp = ExperimentWrapper(init_all=False)
+# this is how seml loads the config file internally
+assert Path(
+    fargs.config
+).exists(), "config file not found"
+seml_config, slurm_config, experiment_config = read_config(
+   fargs.config
+)
+# we take the first config generated
+configs = generate_configs(experiment_config)
+if len(configs) > 1:
+    print("Careful, more than one config generated from the yaml file")
+args = configs[0]
+pprint(args)
+
+exp.seed = 1337
+# loads the dataset splits
+exp.init_dataset(**args["dataset"])
+
+exp.init_drug_embedding(embedding=args["model"]["embedding"])
+exp.init_model(
+    hparams=args["model"]["hparams"],
+    additional_params=args["model"]["additional_params"],
+    load_pretrained=args["model"]["load_pretrained"],
+    append_ae_layer=args["model"]["append_ae_layer"],
+    enable_cpa_mode=args["model"]["enable_cpa_mode"],
+    pretrained_model_path=args["model"]["pretrained_model_path"],
+    pretrained_model_hashes=args["model"]["pretrained_model_hashes"],
+)
+
+exp.update_datasets()
+
+# train the model many times and save the results
+for num in range(fargs.num_trainings):
+    train_results = exp.train(**args["training"], save_name=f"{fargs.save[-3]}_{num}.pt")
+    print(f"Training {num} finished. Printing results:", train_results)
 
